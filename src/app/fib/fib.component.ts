af61931c3cd8b52f11aca8c0c3ac14c25fabfa79
Mike Parks <mparks@bullhorn.com>
2016-10-23 05:35:58 -0500

    any position of the fibonacci sequence is now available

--- src/app/fib/fib.component.ts
+++ src/app/fib/fib.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-fib',
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fib.component.css']
 })
 export class FibComponent implements OnInit {
+  @Input() value: number;
   result: number;
-  value: number;
   position: number;
 
   constructor() { }
@@ -18,22 +18,25 @@ export class FibComponent implements OnInit {
     this.position = 0;
   }
 
-  getNumber(input) {
+  ngOnChanges() {
+    this.getNumber();
+  }
+
+  getNumber() {
     this.position = 1;
-    if (input.value == 0) {
+    if (this.value == 0) {
       this.result = 0;
     } else {
-      this.result = this.doFib(1, 1, input.value);
-      console.log('this.result', this.result);
+      this.result = this.doFib(1, 1);
     }
   }
 
-  doFib(last: number, current: number, stop: number) {
-    let newCurrent: number = stop == 1 ? 1 : last + current;
-    if (this.position < stop) {
+  doFib(last: number, current: number) {
+    let newCurrent: number = this.value == 1 ? 1 : last + current;
+    if (this.position < this.value) {
       console.log('last, current', last, current);
       this.position++;
-      return this.doFib(current, newCurrent, stop);
+      return this.doFib(current, newCurrent);
     } else {
       return current;
     }
